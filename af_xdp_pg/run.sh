0eb1adbe40d9cea9e76d8f5abb8f874fc7c637e6
Qi Zhang <qi.z.zhang@intel.com>
2018-02-24 02:32:33 +0800

    cleanup Makefile
    Signed-off-by: Qi Zhang <qi.z.zhang@intel.com>

--- af_xdp_pg/run.sh
+++ af_xdp_pg/run.sh
@@ -1,3 +1,2 @@
-#./bind.sh $1
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib64
 ../build/app/testpmd -c 0x3 -n 4 --vdev 'eth_af_xdp,iface='$1'' -- -i --mbuf-size=1856 --mp-flags=65 --eth-peer=0,08:00:27:31:24:36
-#./unbind.sh $1
--- mk/rte.app.mk
+++ mk/rte.app.mk
@@ -120,7 +120,7 @@ ifeq ($(CONFIG_RTE_BUILD_SHARED_LIB),n)
 _LDLIBS-$(CONFIG_RTE_DRIVER_MEMPOOL_STACK)  += -lrte_mempool_stack
 
 _LDLIBS-$(CONFIG_RTE_LIBRTE_PMD_AF_PACKET)  += -lrte_pmd_af_packet
-_LDLIBS-$(CONFIG_RTE_LIBRTE_PMD_AF_XDP)     += -lrte_pmd_af_xdp -lelf -L$(TOOLS_DIR)/lib/bpf -lbpf
+_LDLIBS-$(CONFIG_RTE_LIBRTE_PMD_AF_XDP)     += -lrte_pmd_af_xdp -lelf -lbpf
 _LDLIBS-$(CONFIG_RTE_LIBRTE_ARK_PMD)        += -lrte_pmd_ark
 _LDLIBS-$(CONFIG_RTE_LIBRTE_AVF_PMD)        += -lrte_pmd_avf
 _LDLIBS-$(CONFIG_RTE_LIBRTE_AVP_PMD)        += -lrte_pmd_avp
