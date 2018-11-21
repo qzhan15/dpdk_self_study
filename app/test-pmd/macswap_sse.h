dfddba5952cee60e8b1f65bd95cb5c972ce0d7fe
Qi Zhang <qi.z.zhang@intel.com>
2018-11-21 08:03:42 +0000

    app/testpmd: improve mac swap performance for x86
    The patch optimizes the mac swap operation by taking advantage
    of SSE instructions, it only impacts x86 platform.
    
    Signed-off-by: Qi Zhang <qi.z.zhang@intel.com>

---
+++ app/test-pmd/macswap_sse.h
@@ -0,0 +1,43 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2018 Intel Corporation
+ */
+
+#ifndef _L2FWD_SSE_H_
+#define _L2FWD_SSE_H_
+
+#include "macswap_common.h"
+static inline void
+do_macswap(struct rte_mbuf *pkts[], uint16_t nb,
+		struct rte_port *txp)
+{
+	struct ether_hdr *eth_hdr;
+	struct rte_mbuf *mb;
+	uint64_t ol_flags;
+	int i;
+	__m128i addr;
+	__m128i shfl_msk = _mm_set_epi8(15, 14, 13, 12,
+					5, 4, 3, 2,
+					1, 0, 11, 10,
+					9, 8, 7, 6);
+
+	ol_flags = ol_flags_init(txp->dev_conf.txmode.offloads);
+
+	for (i = 0; i < nb; i++) {
+		if (likely(i < nb - 1))
+			rte_prefetch0(rte_pktmbuf_mtod(pkts[i+1], void *));
+		mb = pkts[i];
+
+		eth_hdr = rte_pktmbuf_mtod(mb, struct ether_hdr *);
+
+		/* Swap dest and src mac addresses. */
+		addr = _mm_loadu_si128((__m128i *)eth_hdr);
+		addr = _mm_shuffle_epi8(addr, shfl_msk);
+		_mm_storeu_si128((__m128i *)eth_hdr, addr);
+
+		mbuf_field_set(mb, ol_flags, txp->tx_vlan_id,
+				txp->tx_vlan_id_outer);
+	}
+}
+
+#endif /* _BPF_CMD_H_ */
+
